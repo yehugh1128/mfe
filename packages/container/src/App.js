f1902a0173563f90539904be0e7c08956c792a8b
hugo.ye <113670042@qq.com>
2024-01-27 00:00:26 +0800

    test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
     return <div>
-        <h1>Container</h1>
+        <h1>Container!</h1>
         <hr />
         <MarketingApp />
     </div>
