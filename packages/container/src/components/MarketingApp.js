cff113070353f4c2f372edfc0f4dae1789d40299
hugo.ye <113670042@qq.com>
2024-01-28 01:34:16 +0800

    test

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,12 @@
-import { mount } from 'marketing/app';
+// import { mount } from 'marketing/app';
 import React, { useRef, useEffect } from 'react';
+// console.log(mount);
 export default () => {
     const ref = useRef(null);
 
-    useEffect(() => {
-        mount(ref.current);
-    });
+    // useEffect(() => {
+    //     mount(ref.current);
+    // });
 
-    return <div ref={ref}></div>
+    return <div ref={ref}>abc</div>
 }
