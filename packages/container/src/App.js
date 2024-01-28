4ce053849ea2f42b9c38aafa70b78e9a890991c4
hugo.ye <113670042@qq.com>
2024-01-28 21:33:30 +0800

    test1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 export default () => {
-    return <div>
-        <h1>Container!</h1>
-        <hr />
-        <MarketingApp />
-    </div>
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    );
 }
