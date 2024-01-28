3ecd421e839c2a6e2d1cb9e7d733bc56802ca8d2
hugo.ye <113670042@qq.com>
2024-01-28 19:56:45 +0800

    test

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,11 @@
-import { mount } from 'marketing/MarketingApp';
+import Test from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 export default () => {
     const ref = useRef(null);
 
-    useEffect(() => {
-        mount(ref.current);
-    });
+    // useEffect(() => {
+    //     mount(ref.current);
+    // });
 
-    return <div ref={ref}></div>
+    return <div ref={ref}><Test /></div>
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const mount = (el) => {
-    ReactDOM.render(
-        <App />,
-        el
-    )
-};
-
-if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-    if (devRoot) {
-        mount(devRoot);
-    }
+export default () => {
+    return <h3>abc</h3>
 }
-
-export { mount }
