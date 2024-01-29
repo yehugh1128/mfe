8838185d03d75ec57fa86a5d0b7dbf8c8fec574b
hugo.ye <113670042@qq.com>
2024-01-29 22:17:30 +0800

    added generate class name

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 
+//解决Css样式冲突
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
+
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Routes>
                     <Route exact path='/pricing' Component={Pricing}></Route>
