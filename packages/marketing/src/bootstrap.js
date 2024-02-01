88d9c99b710443430371048f44eb634a5ab8424b
hugo <113670042@qq.com>
2024-02-01 19:16:11 +0800

    test

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory, createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history';
 import App from './App';
-
 const mount = (el, { onNavigate, history }) => {
     if (onNavigate && history) {
         history.listen(onNavigate);
     }
     ReactDOM.render(
-        <App history={history || createMemoryHistory()} />,
+        <App history={history} />,
         el
     );
 };
 
 if (process.env.NODE_ENV === 'development') {
+    const defaultHistory = createBrowserHistory();
     const devRoot = document.querySelector('#_marketing-dev-root');
+    let pathname = '';
     if (devRoot) {
         mount(devRoot, {
-            history: createBrowserHistory()
+            onNavigate: ({ location }) => {
+                if (pathname !== location.pathname) {
+                    pathname = location.pathname;
+                    ReactDOM.render(
+                        <App history={defaultHistory} />,
+                        devRoot
+                    );
+                }
+            },
+            history: defaultHistory
         });
     }
 }
