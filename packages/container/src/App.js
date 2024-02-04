0f4fe0c85cd2ca02efb9ada105f2e7c7a58fb825
hugo <113670042@qq.com>
2024-02-04 17:42:14 +0800

    added dashboard subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,12 @@
-import React, { Suspense, lazy, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const MarketingApp = lazy(() => import('./components/MarketingApp'));
 const AuthApp = lazy(() => import('./components/AuthApp'));
+const DashboardApp = lazy(() => import('./components/DashboardApp'));
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
@@ -18,7 +19,8 @@ export default () => {
                     <Header signedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
                     <Suspense fallback={<Progress />}>
                         <Routes>
-                            <Route path='/auth/*' element={<AuthApp onSignIn={() => setIsSignedIn(true)} />} />
+                            <Route path='/auth/*' element={isSignedIn && <Navigate to="/dashboard" /> || <AuthApp onSignIn={() => setIsSignedIn(true)} />} />
+                            <Route exact path='/dashboard' element={isSignedIn && <DashboardApp /> || <Navigate to="/" />} />
                             <Route path='/*' Component={MarketingApp} />
                         </Routes>
                     </Suspense>
---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,7 @@
+import { mount } from 'dashboard/DashboardApp';
+import React from 'react';
+import Common from './Common';
+
+export default () => {
+    return <Common mount={mount} />
+}
